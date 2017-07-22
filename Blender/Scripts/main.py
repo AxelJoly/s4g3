36d338257aab6e9d8ae42d0cfde2215946989364
s0h3ck <s0h3ck@gmail.com>
2017-07-22 10:43:58 -0400

    Changes for the 'Revue 2'

--- Blender/Scripts/main.py
+++ Blender/Scripts/main.py
@@ -11,7 +11,7 @@ class Curve(object):
     def load_data(self, filename):
         with open(filename, 'r') as file:
             points = [(float(coordinate) for coordinate in line.split()) for line in  file]
-            self.array_vector = [Vector(x) for x in points]
+            self.array_vector = [Vector(x)*2 for x in points]
 
         # print(self.array_vector)
     
@@ -24,7 +24,7 @@ class Curve(object):
         curvedata = bpy.data.curves.new(name="curve_name", type='CURVE')  
         curvedata.dimensions = '3D'  
       
-        objectdata = bpy.data.objects.new("object_name", curvedata)  
+        objectdata = bpy.data.objects.new("Curve", curvedata)  
         objectdata.location = (0,0,0)
         bpy.context.scene.objects.link(objectdata)  
       
@@ -39,8 +39,24 @@ class Curve(object):
         polyline.use_endpoint_u = True
         polyline.use_cyclic_u = True 
 
-    def add_track(self, obj):
-        true_I_useless_for_now = True
+    def add_track(self, filename):
+        models_path = os.path.join(base_path, "Models", filename)
+    
+        imported_object = bpy.ops.import_scene.obj(filepath=models_path)
+        rail = bpy.context.selected_objects[0]
+        rail.name = "Rail"
+        rail.dimensions = (rail.dimensions/10)
+        
+        bpy.context.scene.objects.active = rail
+        
+        bpy.ops.object.modifier_add(type='ARRAY')
+        
+        bpy.context.object.modifiers['Array'].count = 20
+        bpy.ops.object.modifier_add(type='CURVE')
+        bpy.context.object.modifiers['Curve'].object = bpy.data.objects['Curve']
+        
+        bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Array")
+        bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Curve")
 
 def remove_all_objects():
     for o in bpy.data.objects:
@@ -50,41 +66,21 @@ def remove_all_objects():
 
 if __name__ == "__main__":
     os.system('cls')
+
+    #try:
+    #    objs = bpy.data.objects
+    #    objs.remove(objs["Rail"], True)
+    #    objs.remove(objs["Curve"], True)
+    #except:
+    #    pass
     
-    remove_all_objects()
+    #base_path = os.path.dirname(os.path.dirname(__file__)) # base_path
+    #data_path = os.path.join(base_path, "Data", "data_test")
     
-    base_path = os.path.dirname(os.path.dirname(__file__)) # base_path
-    data_path = os.path.join(base_path, "Data", "data_test")
+    #curve = Curve()
+    #curve.load_data(data_path)
+    #curve.draw()
+    #curve.add_track("rail.obj")
     
-    curve = Curve()
-    curve.load_data(data_path)
-    curve.draw()
+    
         
-    models_path = os.path.join(base_path, "Models", "rail.obj")
-    
-    imported_object = bpy.ops.import_scene.obj(filepath=models_path)
-    rail = bpy.context.selected_objects[0]
-    rail.name = "Rail"
-    rail.dimensions = (rail.dimensions/10)
-    
-    bpy.context.scene.objects.active = rail
-    
-    bpy.ops.object.modifier_add(type='ARRAY')
-    
-    bpy.context.object.modifiers['Array'].count = 89
-    bpy.ops.object.modifier_add(type='CURVE')
-    bpy.context.object.modifiers['Curve'].object = bpy.data.objects['object_name']
-    
-    bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Array")
-    bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Curve")
-        
-    #models_path = os.path.join(base_path, "Models", "train.obj")
-    
-    #imported_object = bpy.ops.import_scene.obj(filepath=models_path)
-    bpy.ops.mesh.primitive_cube_add(radius=1)
-    
-    tren = bpy.data.objects["Cube"]
-    bpy.context.scene.objects.active = tren
-    
-    bpy.ops.object.constraint_add(type='FOLLOW_PATH')
-    bpy.context.object.constraints["Follow Path"].target = bpy.data.objects["object_name"]
